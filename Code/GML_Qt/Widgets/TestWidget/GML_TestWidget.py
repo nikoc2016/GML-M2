5a7f89a621dea92446051be30e4032ec4de384d9
NikoC <nikoc@github.com>
2023-06-27 22:18:33 +0800

    Wrong Version Removal

--- Code/GML_Qt/Widgets/TestWidget/GML_TestWidget.py
+++
@@ -1,3 +0,0 @@
-# coding=utf8
-# Copyright (c) lovICer 2021/6/22 GVF
-
--- Code/GML_Qt/Widgets/__init__.py
+++
@@ -1,3 +0,0 @@
-# Classes
-from GML_Qt.Widgets.SuperWidgets.GML_Widget import GML_Widget
-from GML_Qt.Widgets.MainWindow.MainWindow import GML_MainWindow
